37576bf5b450ad09cf6f8763a4be0ed76068e705
Ahmad Firaz <ahmadfiraz@gmail.com>
2020-08-06 22:07:28 +0700

    add invalid token in error handling

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -13,7 +13,7 @@ function errorHandler(err, req, res, next){
     } else if (err.errorCode == 'NOT_FOUND'){
         statusCode = 404
         message = 'Data not Found'
-    } else if (err.errorCode == 'INCORRECT_USER' || err.name == 'JsonWebTokenError'){
+    } else if (err.errorCode == 'INCORRECT_USER'){
         statusCode = 401
         message = 'Username or Password incorrect'
     } else if (err.errorCode == 'NOT_AUTHENTICATED'){
@@ -22,6 +22,9 @@ function errorHandler(err, req, res, next){
     } else if (err.errorCode == 'FORBIDDEN_REQUEST'){
         statusCode = 403
         message = 'Not authorize'
+    } else if (err.code == 409){
+        statusCode = 409
+        message = 'Invalid Token'
     }
     return res.status(statusCode).json({message})
 }
