8cc3494d56ad429b7bb5f1272e213c935301200a
Ahmad Firaz <ahmadfiraz@gmail.com>
2020-08-06 22:05:13 +0700

    Update errorHandler file in middlewares

---
+++ server/middlewares/error.js
@@ -0,0 +1,30 @@
+function errorHandler(err, req, res, next){
+    let statusCode = 500
+    let message = 'INTERNAL SERVER ERROR'
+
+    if (err.name == 'SequelizeValidationError'){
+        statusCode = 400
+        const arrError = []
+        const errors = err.errors
+        errors.forEach(el => {
+            arrError.push(el.message)
+        });
+        message = arrError
+    } else if (err.errorCode == 'NOT_FOUND'){
+        statusCode = 404
+        message = 'Data not Found'
+    } else if (err.errorCode == 'INCORRECT_USER' || err.name == 'JsonWebTokenError'){
+        statusCode = 401
+        message = 'Username or Password incorrect'
+    } else if (err.errorCode == 'NOT_AUTHENTICATED'){
+        statusCode = 401
+        message = 'Not authenticated'
+    } else if (err.errorCode == 'FORBIDDEN_REQUEST'){
+        statusCode = 403
+        message = 'Not authorize'
+    }
+    return res.status(statusCode).json({message})
+}
+
+module.exports = errorHandler
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
 const { UserController } = require('../controllers');
 const { Auth } = require('../middlewares');
+
+router.get('/user', UserController.)
